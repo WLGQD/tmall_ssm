6b5ce89b3ae20e61a3a9262e7a845f4af5dc10f7
WLGQD <xiekaikai89@126.com>
2018-01-26 00:34:47 +0800

    1

--- src/main/java/com/how2java/tmall/service/impl/PropertyServiceImpl.java
+++ src/main/java/com/how2java/tmall/service/impl/PropertyServiceImpl.java
@@ -1,34 +1,44 @@
 package com.how2java.tmall.service.impl;
 
+import com.how2java.tmall.mapper.PropertyMapper;
 import com.how2java.tmall.pojo.Property;
+import com.how2java.tmall.pojo.PropertyExample;
 import com.how2java.tmall.service.PropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public class PropertyServiceImpl implements PropertyService {
-    @Override
-    public void add(Property c) {
+     @Autowired
+    PropertyMapper propertyMapper;
 
+    @Override
+    public void add(Property p) {
+        propertyMapper.insert(p);
     }
 
     @Override
     public void delete(int id) {
-
+        propertyMapper.deleteByPrimaryKey(id);
     }
 
     @Override
-    public void update(Property c) {
-
+    public void update(Property p) {
+        propertyMapper.updateByPrimaryKeySelective(p);
     }
 
     @Override
     public Property get(int id) {
-        return null;
+        return propertyMapper.selectByPrimaryKey(id);
     }
 
     @Override
     public List list(int cid) {
-        return null;
+        PropertyExample example =new PropertyExample();
+        example.createCriteria().andCidEqualTo(cid);
+        example.setOrderByClause("id desc");
+        return propertyMapper.selectByExample(example);
     }
+
 }
