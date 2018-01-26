557a3924cb1fdb85eed5a7d5d88b1da5ac387277
WLGQD <xiekaikai89@126.com>
2018-01-27 00:35:33 +0800

    1

--- src/main/java/com/how2java/tmall/controller/ProductController.java
+++ src/main/java/com/how2java/tmall/controller/ProductController.java
@@ -1,19 +1,11 @@
-/**
- * 模仿天猫整站ssm 教程 为how2j.cn 版权所有
- * 本教程仅用于学习使用，切勿用于非法用途，由此引起一切后果与本站无关
- * 供购买者学习，请勿私自传播，否则自行承担相关法律责任
- */
-
 package com.how2java.tmall.controller;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.how2java.tmall.pojo.Category;
 import com.how2java.tmall.pojo.Product;
-import com.how2java.tmall.pojo.Property;
 import com.how2java.tmall.service.CategoryService;
 import com.how2java.tmall.service.ProductService;
-import com.how2java.tmall.service.PropertyService;
 import com.how2java.tmall.util.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
